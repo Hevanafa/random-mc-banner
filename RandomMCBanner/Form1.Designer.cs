1643f8cc032d68549195fce3cf6fe8b8f41168da
Hevanafa <dhevansafa4@gmail.com>
2021-06-18 05:55:50 +0700

    First upload

---
+++ RandomMCBanner/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace RandomMCBanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudPatternDepth = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbDisplayName = new System.Windows.Forms.TextBox();
+            this.txbLore = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPatternDepth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudPatternDepth
+            // 
+            this.nudPatternDepth.Location = new System.Drawing.Point(12, 28);
+            this.nudPatternDepth.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudPatternDepth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudPatternDepth.Name = "nudPatternDepth";
+            this.nudPatternDepth.Size = new System.Drawing.Size(80, 23);
+            this.nudPatternDepth.TabIndex = 0;
+            this.nudPatternDepth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudPatternDepth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pattern count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output";
+            // 
+            // txbOutput
+            // 
+            this.txbOutput.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbOutput.Location = new System.Drawing.Point(12, 73);
+            this.txbOutput.Multiline = true;
+            this.txbOutput.Name = "txbOutput";
+            this.txbOutput.ReadOnly = true;
+            this.txbOutput.Size = new System.Drawing.Size(373, 150);
+            this.txbOutput.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Display name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Lore";
+            // 
+            // txbDisplayName
+            // 
+            this.txbDisplayName.Location = new System.Drawing.Point(98, 28);
+            this.txbDisplayName.Name = "txbDisplayName";
+            this.txbDisplayName.Size = new System.Drawing.Size(100, 23);
+            this.txbDisplayName.TabIndex = 6;
+            // 
+            // txbLore
+            // 
+            this.txbLore.Location = new System.Drawing.Point(204, 28);
+            this.txbLore.Name = "txbLore";
+            this.txbLore.Size = new System.Drawing.Size(100, 23);
+            this.txbLore.TabIndex = 7;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(310, 27);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 24);
+            this.btnGenerate.TabIndex = 8;
+            this.btnGenerate.Text = "Generate!";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Minecraft version: Java 1.16";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(398, 257);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txbLore);
+            this.Controls.Add(this.txbDisplayName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudPatternDepth);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Minecraft Random Banner Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.nudPatternDepth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudPatternDepth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbDisplayName;
+        private System.Windows.Forms.TextBox txbLore;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RandomMCBanner/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RandomMCBanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1() { InitializeComponent(); }
+
+        //{
+        //    "white", // 0
+        //    "orange",
+        //    "magenta",
+        //    "light_blue",
+        //    "yellow", // 4
+        //    "lime",
+        //    "pink",
+        //    "gray",
+        //    "light_gray", // 8
+        //    "cyan",
+        //    "purple",
+        //    "blue",
+        //    "brown", // 12
+        //    "green",
+        //    "red",
+        //    "black"
+        //};
+        readonly string[] colours = "white,orange,magenta,light_blue,yellow,lime,pink,gray,light_gray,cyan,purple,blue,brown,green,red,black".Split(',');
+        readonly string[] patterns = "b,bs,ts,ls,rs,cs,ms,drs,dls,ss,cr,sc,ld,rud,lud,rd,vh,vhr,hh,hhb,bl,br,tl,tr,bt,tt,bts,tts,mc,mr,bo,cbo,bri,gra,gru,cre,sku,flo,moj,glb,pig".Split(',');
+        
+		
+		// Basic syntax:
+		// https://www.digminecraft.com/generators/give_banner_1_16.php
+		
+		// Colour list:
+		// https://www.digminecraft.com/lists/pattern_color_list_pc.php
+		
+		// Pattern list:
+		// https://minecraft.fandom.com/wiki/Banner/Patterns
+		
+		// Banner reference:
+		// https://minecraft.tools/en/img/outils/bannersx4.png
+
+        Random r = new Random();
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            int depth = (int)nudPatternDepth.Value;
+
+            var sb = new StringBuilder();
+
+            sb.Append("/give @p ");
+            sb.Append($"{colours[r.Next(0, 16)]}_banner{{");
+
+            var displayName = txbDisplayName.Text.Trim();
+            var lore = txbLore.Text.Trim();
+            if (displayName.Length > 0 || lore.Length > 0)
+                sb.Append($"display: {{ Name: \"\\\"{ displayName }\\\"\", Lore: [\"\\\"{ lore }\\\"\"]}}, ");
+
+            sb.Append("BlockEntityTag: { Patterns: [ ");
+
+            var patternList = new List<string>();
+            string pattern;
+            int colour;
+            for (var a = 0; a < depth; a++)
+            {
+                pattern = patterns[r.Next(0, patterns.Length)];
+                colour = r.Next(0, 16);
+                patternList.Add($"{{ Pattern: \"{pattern}\", Color: {colour} }}");
+            }
+            sb.Append(string.Join(", ", patternList));
+
+			// the amount of items
+            sb.Append(" ]}} 1");
+
+            txbOutput.Text = sb.ToString();
+        }
+    }
+}
